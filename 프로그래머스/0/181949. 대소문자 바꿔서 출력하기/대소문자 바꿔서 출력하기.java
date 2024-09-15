2e682616efec980f6c373c3aaa6ea8f7adbfdb17
superuser1241 <137857174+superuser1241@users.noreply.github.com>
2024-09-15 16:05:13 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 370.67 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+        String result = "";
+        
+        for (int i = 0; i < a.length(); i++) {
+            char ch = a.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                result += (char)(ch + 32);
+            }
+            else if (ch >= 'a' && ch <= 'z') {
+                result += (char)(ch - 32);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
